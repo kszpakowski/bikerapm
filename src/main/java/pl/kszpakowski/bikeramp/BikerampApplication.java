ff8d539edca942e807f273871d49545d7315fec5
Karol Szpakowski <karol.szpakowski@gmail.com>
2021-03-21 13:20:29 +0100

    initial project scaffold

---
+++ src/main/java/pl/kszpakowski/bikeramp/BikerampApplication.java
@@ -0,0 +1,13 @@
+package pl.kszpakowski.bikeramp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikerampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikerampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kszpakowski/bikeramp/BikerampApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kszpakowski.bikeramp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikerampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
